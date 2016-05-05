4e0b50f5f736de46b8b3670528ea53d34a45f864
sioanis <sueann.ioanis@gmail.com>
2016-05-05 04:31:31 -0700

    Fix angular build min error.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -54,7 +54,7 @@ gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('app/scripts/**/*.js', $.uglify()))
     .pipe($.if('*.css', $.cssnano()))
     .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
